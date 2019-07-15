541214470c33ea7a85a507e22c293d2fee1e256c
cwern40 <chris.wernli@hotmail.com>
2019-07-15 16:32:11 -0600

    finished the todo form and adjusted the reducer so that it adds the new item to state.

--- todo/src/Components/TodoForm.js
+++ todo/src/Components/TodoForm.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { addTodo } from '../Action';
+import { connect } from 'react-redux';
 
 class TodoForm extends React.Component {
     constructor (){
         super();
         this.state= {
-        value: '',
-        completed: false,        
+                value: '',
+                completed: false,       
         }
     }
 
     changeHandler = (event) => {
-        event.preventDefault()
-
         this.setState({[event.target.name]: event.target.value})
     }
 
+    addItem = (event) => {
+        event.preventDefault();
+        this.props.addTodo(this.state.value, this.state.completed)
+    }
+
     render() {
+        console.log('form', this.state);
         return(
-            <form>
+            <form onSubmit={this.addItem}>
                 <input type="text" placeholder="enter your task here..." name="value" className="value-input" value={this.state.value} onChange={this.changeHandler}/>
                 <button>Add Todo Item</button>
             </form>
@@ -25,4 +31,20 @@ class TodoForm extends React.Component {
     }
 }
 
-export default TodoForm;
+// const mapDispatchToProps = (dispatch) => {
+// 	return {
+// 		makeDeposit: (amount, account) => dispatch(makeDeposit(amount, account))
+// 	}
+// }
+
+const mapStateToProps = (state) => {
+    return {
+        todos: state.todos
+    }
+}
+
+const mapDispatchToProps = {
+    addTodo: addTodo
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
